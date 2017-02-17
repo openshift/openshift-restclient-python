6506dcc57363d48f2f016a204b7eb6f75a37a649
Chris Houseknecht <chousekn@redhat.com>
2017-02-17 05:06:23 -0500

    Adds argspec property to openshift.helper

--- openshift/helper/__init__.py
+++ openshift/helper/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import inspect
 import re
 
@@ -8,19 +10,45 @@ from kubernetes.client.rest import ApiException
 from openshift import client
 from openshift.client.models import V1DeleteOptions
 
-#: Regex for finding versions
+# Regex for finding versions
 VERSION_RX = re.compile("V\d((alpha|beta)\d)?")
+
+# Expected metaclass object name
+METACLASS_NAME = 'V1ObjectMeta'
+
+
+class OpenShiftException(Exception):
+    """
+    Raised when there is an error inside of an Ansible module.
+    """
+
+    def __init__(self, message, **kwargs):
+        """
+        Creates an instance of the AnsibleModuleError class.
+        :param message: The friendly error message.
+        :type message: basestring
+        :param kwargs: All other keyword arguments.
+        :type kwargs: dict
+        """
+        self.message = message
+        self.value = kwargs
+        self.value['message'] = message
+
+    def __str__(self):
+        """
+        String representation of the instance.
+        """
+        return json.dumps(self.value)
 
 
 class KubernetesObjectHelper(object):
     # TODO: add support for check mode to helper
-    def __init__(self, api_version, kind):
+    def __init__(self, api_version, kind, namespaced=False):
         self.api_version = api_version
         self.kind = kind
         self.model = self.__get_model(api_version, kind)
-
-        model_obj = self.model()
-        self.properties = self.properties_from_model_obj(model_obj)
+        self.properties = self.properties_from_model_obj(self.model())
+        self.namespaced = namespaced
 
         # TODO: handle config better than just using default kubeconfig
         config.load_kube_config()
@@ -92,7 +120,7 @@ class KubernetesObjectHelper(object):
         properties = {}
         for name in property_names:
             prop_kind = model_obj.swagger_types[name]
-            if prop_kind == 'str':
+            if prop_kind in 'str':
                 prop_class = str
             elif prop_kind == 'int':
                 prop_class = int
@@ -100,13 +128,21 @@ class KubernetesObjectHelper(object):
                 prop_class = list
             elif prop_kind.startswith('dict('):
                 prop_class = dict
+            elif prop_kind == 'bool':
+                prop_class = bool
             else:
                 prop_class = getattr(client.models, prop_kind)
-
+            # cls.properties_from_model_obj(())
             properties[name] = prop_class
         return properties
 
     def __lookup_method(self, operation, namespaced):
+        '''
+
+        :param operation:
+        :param namespaced:
+        :return:
+        '''
         # TODO: raise error if method not found
         method_name = operation
         if namespaced:
@@ -128,7 +164,88 @@ class KubernetesObjectHelper(object):
 
     @staticmethod
     def __get_model(api_version, kind):
+        '''
+        Return the model class for the requested object.
+
+        :param api_version: API version string
+        :param kind: The name of object type (i.e. Service, Route, Container, etc.)
+        :return: class
+        '''
         camel_kind = string_utils.snake_case_to_camel(kind).capitalize()
         model_name = api_version.capitalize() + camel_kind
         model = getattr(client.models, model_name)
         return model
+
+    @property
+    def argspec(self):
+        '''
+        Introspect the model properties, and return an Ansible module arg_spec dict.
+
+        :return: dict
+        '''
+        argument_spec = {
+            'state': {
+                'default': 'present',
+                'choices': ['present', 'absent']
+            },
+            'name': {
+                'required': True
+            },
+            # path to a config file
+            'kubeconfig': {},
+
+            # kubectl context name
+            'context': {}
+        }
+
+        if 'metadata' not in self.properties.keys():
+            raise OpenShiftException(
+                "Object {} does not contain metadata field".format(self.model)
+            )
+
+        if self.namespaced:
+            argument_spec['namespace'] = {'required': True}
+
+        # mutually_exclusive = None
+        # required_together = None
+        # required_one_of = None
+        # required_if = None
+
+        argument_spec.update(self.__transform_properties(self.properties))
+        return argument_spec
+
+    def __transform_properties(self, properties, prefix=None):
+        '''
+        Convert a list of properties to an argument_spec dictionary
+
+        :param properties: List of properties from self.properties_from_model_obj()
+        :param prefix: String to prefix to argument names.
+        :return: dict
+        '''
+        args = {}
+        print("here!")
+        for prop, prop_class in properties.items():
+            print("property: {}".format(prop))
+            if prop == 'status':
+                # Do not expose Status to argument spec
+                continue
+            elif prop == 'metadata':
+                # Filter metadata properties added to argument spec
+                if prop_class.__name__ != METACLASS_NAME:
+                    raise OpenShiftException("Unknown metadata type: {}".format(prop_class.__name__))
+                args['labels'] = {'required': False, 'type': 'dict'}
+                args['annotations'] = {'required': False, 'type': 'dict'}
+            elif prop_class.__name__ not in ['int', 'str', 'bool', 'list', 'dict']:
+                # Adds nested properties
+                p = prefix
+                if prop not in ('spec', 'template'):
+                    # Only prefix property names with meaningful terms
+                    p = prefix + '_' if prefix else ''
+                    p += prop
+                sub_props = self.properties_from_model_obj(prop_class())
+                args.update(self.__transform_properties(sub_props, prefix=p))
+            else:
+                # Adds a primitive property
+                arg_prefix = prefix + '_' if prefix else ''
+                args[arg_prefix + prop] = { 'required': False, 'type': prop_class.__name__}
+        return args
