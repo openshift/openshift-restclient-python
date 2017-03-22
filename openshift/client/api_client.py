ac02f4b1fa995162064b47534b1adeb18aeca1e5
Jason DeTiberus <jdetiber@redhat.com>
2017-03-22 12:18:06 -0400

    fix for api_client

--- openshift/client/api_client.py
+++ openshift/client/api_client.py
@@ -1,9 +1,14 @@
+from __future__ import absolute_import
+
 from kubernetes.client.api_client import ApiClient as K8sApiClient
+
+from . import models
 
 class ApiClient(K8sApiClient):
     def _ApiClient__deserialize(self, data, klass):
         try:
             return super(ApiClient, self).__deserialize(data, klass)
         except AttributeError:
-            klass = eval('models.' + klass)
+            klass = getattr(models, klass)
             return super(ApiClient, self).__deserialize_model(data, klass)
+
