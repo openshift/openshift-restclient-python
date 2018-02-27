50b1b144f4bf29bcface3e822178763b0e40dfbc
Fabian von Feilitzsch <fabian@fabianism.us>
2018-02-27 14:54:20 -0500

    Version bump

--- openshift/__init__.py
+++ openshift/__init__.py
@@ -18,5 +18,5 @@ import kubernetes.watch
 
 # Do not edit these constants. They will be updated automatically
 # by scripts/update-client.sh.
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 __k8s_client_version__ = "4.0.0"
