8ac664822fd26fd19c741c0be62eb3c7d0bd2847
Fabian von Feilitzsch <fabian@fabianism.us>
2018-04-26 17:25:01 -0400

    cleanup, better UI for getting resources

--- openshift/dynamic/client.py
+++ openshift/dynamic/client.py
@@ -12,170 +12,21 @@ from kubernetes.client.api_client import ApiClient
 from kubernetes.client.rest import ApiException
 
 
-class Resource(object):
-
-    def __init__(self, prefix=None, group=None, apiversion=None, kind=None,
-                 namespaced=False, verbs=None, name=None, preferred=False, client=None,
-                 singularName=None, shortNames=None, categories=None, **kwargs):
-
-        if None in (apiversion, kind):
-            raise Exception("At least kind and apiversion must be provided")
-
-        self._prefix = prefix
-        self.group = group
-        self.apiversion = apiversion
-        self.kind = kind
-        self.namespaced = namespaced
-        self.verbs = verbs
-        self.name = name
-        self.preferred = preferred
-        self.client = client
-        self.singular_name = singularName
-        self.short_names = shortNames
-        self.categories = categories
-
-        self.extra_args = kwargs
-
-    def __repr__(self):
-        if self.group:
-            groupversion = '{}/{}'.format(self.group, self.apiversion)
-        else:
-            groupversion = self.apiversion
-        return '<{}({}.{}>)'.format(self.__class__.__name__, groupversion, self.kind)
-
-    @staticmethod
-    def make_resource(prefix, group, apiversion, resource, preferred=False, client=None):
-        if prefix == 'oapi':
-            return OpenshiftResource(prefix=prefix, group=group, apiversion=apiversion, client=client, preferred=preferred, **resource)
-        else:
-            return K8sResource(prefix=prefix, group=group, apiversion=apiversion, client=client, preferred=preferred, **resource)
-
-    @property
-    def prefix(self):
-        if self._prefix:
-            return self._prefix
-        raise NotImplementedError
-
-    @property
-    def urls(self):
-        if self.group:
-            full_prefix = '{}/{}/{}'.format(self.prefix, self.group, self.apiversion)
-        else:
-            full_prefix = '{}/{}'.format(self.prefix, self.apiversion)
-        return {
-            'base': '/{}/{}'.format(full_prefix, self.name),
-            'namespaced_base': '/{}/namespaces/{{namespace}}/{}'.format(full_prefix, self.name),
-            'full': '/{}/{}/{{name}}'.format(full_prefix, self.name),
-            'namespaced_full': '/{}/namespaces/{{namespace}}/{}/{{name}}'.format(full_prefix, self.name)
-        }
-
-    def __getattr__(self, name):
-        return partial(getattr(self.client, name), self)
-
-
-class ResourceField(object):
-
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-    def __repr__(self):
-        return pformat(self.__dict__)
-
-    def __eq__(self, other):
-        return self.__dict__ == other.__dict__
-
-    def __getitem__(self, name):
-        return self.__dict__[name]
-
-    def __dir__(self):
-        return dir(type(self)) + list(self.__dict__.keys())
-
-
-class ResourceInstance(object):
-
-    def __init__(self, resource, instance):
-        self.resource_type = resource
-        self.attributes = self.__deserialize(instance)
-
-    def __deserialize(self, field):
-        if isinstance(field, dict):
-            return ResourceField(**{
-                k: self.__deserialize(v) for k, v in field.items()
-            })
-        elif isinstance(field, (list, tuple)):
-            return [self.__deserialize(item) for item in field]
-        else:
-            return field
-
-    def __serialize(self, field):
-        if isinstance(field, ResourceField):
-            return {
-                k: self.__serialize(v) for k, v in field.__dict__.items()
-            }
-        elif isinstance(field, (list, tuple)):
-            return [self.__serialize(item) for item in field]
-        else:
-            return field
-
-    def to_dict(self):
-        return self.__serialize(self.attributes)
-
-    def to_str(self):
-        return repr(self)
-
-    def __repr__(self):
-        return "ResourceInstance[{}]:\n  {}".format(
-            self.attributes.kind,
-            '  '.join(yaml.safe_dump(self.to_dict()).splitlines(True))
-        )
-
-    def __getattr__(self, name):
-        return getattr(self.attributes, name)
-
-    def __getitem__(self, name):
-        return self.attributes[name]
-
-    def __dir__(self):
-        return dir(type(self)) + list(self.attributes.__dict__.keys())
-
-
-class K8sResource(Resource):
-
-    @property
-    def prefix(self):
-        if self._prefix:
-            return self._prefix
-        elif self.apiversion == 'v1' and not self.group:
-            return '/api'
-        else:
-            return '/apis'
-
-
-class OpenshiftResource(Resource):
-
-    @property
-    def prefix(self):
-        if self._prefix:
-            return self._prefix
-        elif self.apiversion == 'v1' and not self.group:
-            return '/oapi'
-        else:
-            return '/apis'
-
-
-def flatten(l):
-    return [item for sublist in l for item in sublist]
-
-
 class DynamicClient(object):
 
     def __init__(self, client):
         self.client = client
-        self._groups = self.get_api_groups()
-        self._resources = flatten([self.get_resources_for_group(*group_parts) for group_parts in self._groups])
+        self.__resources = ResourceContainer(self.parse_api_groups())
+
+    @property
+    def resources(self):
+        return self.__resources
 
     def default_groups(self):
-        groups = [('api', '', 'v1', True)]
+        groups = {}
+        groups['api'] = { '': {
+            'v1': self.get_resources_for_apiversion('api', '', 'v1', True)
+        }}
 
         try:
             self.request('get', '/version/openshift')
@@ -184,71 +35,49 @@ class DynamicClient(object):
             is_openshift = False
 
         if is_openshift:
-            groups.append(('oapi', '', 'v1', True))
+            groups['oapi'] = { '': {
+                'v1': self.get_resources_for_apiversion('oapi', '', 'v1', True)
+            }}
         return groups
 
-    def get_api_groups(self):
-        """ Returns a list of API groups in the format:
-            (api_prefix, group_name, version, preferred)
-
-            api_prefix is the url prefix to access the group (usually /apis, but
-            /api for core kubernetes resources and /oapi for core openshift resources)
-
-            group_name and version are the name and version of the group
-
-            preferred is a boolean indicating whether this version of the group is preferred
-        """
+    def parse_api_groups(self):
+        ""
         prefix = 'apis'
         groups_response = self.request('GET', '/{}'.format(prefix))['groups']
 
         groups = self.default_groups()
+        groups[prefix] = {}
 
         for group in groups_response:
-            for version in group['versions']:
-                groups.append([
-                    prefix,
-                    group['name'],
-                    version['version'],
-                    version == group['preferredVersion']
-                ])
-
+            new_group = {}
+            for version_raw in group['versions']:
+                version = version_raw['version']
+                preferred = version_raw == group['preferredVersion']
+                new_group[version] = self.get_resources_for_apiversion(prefix, group['name'], version, preferred)
+            groups[prefix][group['name']] = new_group
         return groups
 
-    def get_resources_for_group(self, prefix, group, apiversion, preferred=False):
-        """ returns the list of resources associated with provided groupVersion"""
+    def get_resources_for_apiversion(self, prefix, group, version, preferred):
+        """ returns a dictionary of resources associated with provided groupVersion"""
 
-        path = '/'.join(filter(None, [prefix, group, apiversion]))
+        resources = {}
+
+        path = '/'.join(filter(None, [prefix, group, version]))
         resources_response = self.request('GET', path)['resources']
 
-        # Filter out subresources
+        # TODO: Filter out subresources for now
         resources_raw = filter(lambda resource: '/' not in resource['name'], resources_response)
 
-        resources = []
         for resource in resources_raw:
-            resources.append(Resource.make_resource(
-                prefix,
-                group,
-                apiversion,
-                resource,
+            resources[resource['kind']] = Resource(
+                prefix=prefix,
+                group=group,
+                apiversion=version,
                 client=self,
-                preferred=preferred
-            ))
+                preferred=preferred,
+                **resource
+            )
         return resources
-
-    def search_resources(self, conditional):
-        """ Takes a conditional test and returns a list of resources that satisfy the test
-            The test should take an object of the Resource type as an argument,
-            and return a boolean
-
-        Ex:
-            def is_namespaces(resource):
-                return resource.name == 'namespaces'
-
-            client.search_resources(is_namespaces)
-
-        will return all resources with the name 'namespaces'
-        """
-        return list(filter(conditional, self._resources))
 
     def list(self, resource, namespace=None):
         path_params = {}
@@ -368,11 +197,178 @@ class DynamicClient(object):
         )[0].data)
 
 
+class Resource(object):
+
+    def __init__(self, prefix=None, group=None, apiversion=None, kind=None,
+                 namespaced=False, verbs=None, name=None, preferred=False, client=None,
+                 singularName=None, shortNames=None, categories=None, **kwargs):
+
+        if None in (apiversion, kind, prefix):
+            raise Exception("At least prefix, kind, and apiversion must be provided")
+
+        self.prefix = prefix
+        self.group = group
+        self.apiversion = apiversion
+        self.kind = kind
+        self.namespaced = namespaced
+        self.verbs = verbs
+        self.name = name
+        self.preferred = preferred
+        self.client = client
+        self.singular_name = singularName
+        self.short_names = shortNames
+        self.categories = categories
+
+        self.extra_args = kwargs
+
+    def __repr__(self):
+        if self.group:
+            groupversion = '{}/{}'.format(self.group, self.apiversion)
+        else:
+            groupversion = self.apiversion
+        return '<{}({}.{}>)'.format(self.__class__.__name__, groupversion, self.kind)
+
+    @property
+    def urls(self):
+        if self.group:
+            full_prefix = '{}/{}/{}'.format(self.prefix, self.group, self.apiversion)
+        else:
+            full_prefix = '{}/{}'.format(self.prefix, self.apiversion)
+        return {
+            'base': '/{}/{}'.format(full_prefix, self.name),
+            'namespaced_base': '/{}/namespaces/{{namespace}}/{}'.format(full_prefix, self.name),
+            'full': '/{}/{}/{{name}}'.format(full_prefix, self.name),
+            'namespaced_full': '/{}/namespaces/{{namespace}}/{}/{{name}}'.format(full_prefix, self.name)
+        }
+
+    def __getattr__(self, name):
+        return partial(getattr(self.client, name), self)
+
+
+class ResourceContainer(object):
+    def __init__(self, resources):
+        self.__resources = resources
+
+    def get(self, **kwargs):
+        results = self.search(**kwargs)
+        if len(results) == 1:
+            return results[0]
+        elif not results:
+            raise Exception('No matches found for {}'.format(kwargs))
+        else:
+            raise Exception('Multiple matches found for {}: {}'.format(kwargs, results))
+
+    def search(self, **kwargs):
+        return self.__search(self.__build_search(**kwargs), self.__resources)
+
+    def __build_search(self, kind=None, api_version=None, prefix=None, **kwargs):
+        if api_version and '/' in api_version:
+            group, version = api_version.split('/')
+            items = [prefix, group, version, kind, kwargs]
+        else:
+            items = [prefix, '*', api_version, kind, kwargs]
+
+        return list(map(lambda x: x or '*', items))
+
+    def __search(self, parts, resources):
+        part = parts[0]
+        if part != '*' and resources.get(part):
+            if isinstance(resources.get(part), dict):
+                return self.__search(parts[1:], resources[part])
+            else:
+                resource = resources.get(part)
+                if parts[1] != '*' and isinstance(parts[1], dict):
+                    for term, value in parts[1].items():
+                        if getattr(resource, term) == value:
+                            return [resource]
+                else:
+                    return [resource]
+        elif part == '*':
+            matches = []
+            for key in resources.keys():
+                matches.extend(self.__search([key] + parts[1:], resources))
+            return matches
+        return []
+
+    def __iter__(self):
+        for _, groups in self.__resources.items():
+            for _, versions in groups.items():
+                for _, resources in versions.items():
+                    for _, resource in resources.items():
+                        yield resource
+
+
+class ResourceField(object):
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __repr__(self):
+        return pformat(self.__dict__)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def __getitem__(self, name):
+        return self.__dict__[name]
+
+    def __dir__(self):
+        return dir(type(self)) + list(self.__dict__.keys())
+
+
+class ResourceInstance(object):
+
+    def __init__(self, resource, instance):
+        self.resource_type = resource
+        self.attributes = self.__deserialize(instance)
+
+    def __deserialize(self, field):
+        if isinstance(field, dict):
+            return ResourceField(**{
+                k: self.__deserialize(v) for k, v in field.items()
+            })
+        elif isinstance(field, (list, tuple)):
+            return [self.__deserialize(item) for item in field]
+        else:
+            return field
+
+    def __serialize(self, field):
+        if isinstance(field, ResourceField):
+            return {
+                k: self.__serialize(v) for k, v in field.__dict__.items()
+            }
+        elif isinstance(field, (list, tuple)):
+            return [self.__serialize(item) for item in field]
+        else:
+            return field
+
+    def to_dict(self):
+        return self.__serialize(self.attributes)
+
+    def to_str(self):
+        return repr(self)
+
+    def __repr__(self):
+        return "ResourceInstance[{}]:\n  {}".format(
+            self.attributes.kind,
+            '  '.join(yaml.safe_dump(self.to_dict()).splitlines(True))
+        )
+
+    def __getattr__(self, name):
+        return getattr(self.attributes, name)
+
+    def __getitem__(self, name):
+        return self.attributes[name]
+
+    def __dir__(self):
+        return dir(type(self)) + list(self.attributes.__dict__.keys())
+
+
 def main():
     config.load_kube_config()
     client = DynamicClient(ApiClient())
     ret = {}
-    for resource in client._resources:
+    for resource in client.resources:
         if resource.namespaced:
             key = resource.urls['namespaced_full']
         else:
