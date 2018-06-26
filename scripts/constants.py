85f3fdaf18afc708fb94cb2af33d4fed05bad37f
Fabian von Feilitzsch <fabian@fabianism.us>
2018-06-26 16:19:51 -0400

    Release 0.6.1

--- scripts/constants.py
+++ scripts/constants.py
@@ -23,7 +23,7 @@ SPEC_VERSION = "v3.9.0"
 
 # client version for packaging and releasing. It can
 # be different than SPEC_VERSION.
-CLIENT_VERSION = "0.6.0"
+CLIENT_VERSION = "0.6.1"
 KUBERNETES_CLIENT_VERSION = "6.0.0"
 
 # Name of the release package
---
+++ scripts/update-version.sh
@@ -0,0 +1,9 @@
+set -x
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
+SOURCE_ROOT="${SCRIPT_ROOT}/../"
+CLIENT_ROOT="${SOURCE_ROOT}/${PACKAGE_NAME}"
+CLIENT_VERSION=$(python "${SCRIPT_ROOT}/constants.py" CLIENT_VERSION)
+echo "--- updating version information..."
+sed -i'' "s/^CLIENT_VERSION = .*/CLIENT_VERSION = \\\"${CLIENT_VERSION}\\\"/" "${SCRIPT_ROOT}/../setup.py"
+sed -i'' "s/^__version__ = .*/__version__ = \\\"${CLIENT_VERSION}\\\"/" "${CLIENT_ROOT}/__init__.py"
+sed -i'' "s/^Version:.*/Version:    ${CLIENT_VERSION}/" "${SCRIPT_ROOT}/../python-openshift.spec"
