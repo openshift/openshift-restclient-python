79901616c54fb65e4c64c8597ec26b7182792872
Fabian von Feilitzsch <fabian@fabianism.us>
2019-01-11 14:56:35 -0500

    hash cache name to prevent it from becoming too long

--- openshift/dynamic/client.py
+++ openshift/dynamic/client.py
@@ -1,7 +1,7 @@
 import os
 import copy
 import json
-import base64
+import hashlib
 import tempfile
 from functools import partial
 from abc import abstractmethod, abstractproperty
@@ -581,7 +581,7 @@ class Discoverer(object):
         default_cache_id = self.client.configuration.host
         if six.PY3:
             default_cache_id = default_cache_id.encode('utf-8')
-        default_cachefile_name = 'osrcp-{0}.json'.format(base64.b64encode(default_cache_id).decode('utf-8'))
+        default_cachefile_name = 'osrcp-{0}.json'.format(hashlib.md5(default_cache_id).hexdigest())
         self.__cache_file = cache_file or os.path.join(tempfile.gettempdir(), default_cachefile_name)
         self.__init_cache()
 
